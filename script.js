4c7c579a89a79a464ecce8e5040d56627ac08619
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-01 11:34:50 -0700

    Added elements to HTML for start page

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+function startPlay() {
+document.getElementById('startButton').style.display="block"
+var startButton = document.getElementById("#startButton")
+startButton.addEventlistener("clicked", function())
+console.log("clicked")
+
+   
+} 
+// once button clicked, page changes to word game block
+
+var  word = "";
+var wordGuess = [];
+var wrongGuess = [];
+var 
+
+//
+
 //Timer element
-/*var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
+var timerEl = document.getElementById('#timer');
+/*var mainEl = document.getElementById('main');
 
 //var message = 
+*/
 function countdown() {
     var timeLeft = 60;
     var timeInterval = setInterval(function () {
@@ -14,21 +32,35 @@ function countdown() {
         timerEl.textContent = timeLeft + ' seconds remaining';
         timeLeft--;
     } 
-},60000); 
+},60000); }
 
 */
-var listOfWords = ["j,a,v,a,s,c,r,i,p,t"];
-var selectedWord;
 
 
+
+//generate random word from list of words in the array
 function randomWord () {
+
+    var listOfWords = ["javascript", "computer", "syntax", "assignment", "bootcamp", "coding"];
+    var selectedWord;
+
     var randomIndex = Math.floor(Math.random() * listOfWords.Length)
     return selectedWord = listOfWords[randomIndex]};
-
-
-    randomWord();
     console.log(selectedWord);
+
  //   console.log(wordArray);
 
 var wordArray = selectedWord.split(",");
 console.log(listOfWords)
+
+//to display key pressed on the screen
+
+function keydownAction(event) {
+    console.log(event);
+    var keyPress = event.key;
+    
+
+    document.querySelector("#key").textContent = keyPress;
+    document.addEventListener("keydown", keydownAction);
+
+}
